36e387315b53c8ee1942e02c1293567126cb03ae
mayumar <maytalbada@gmail.com>
2024-04-06 21:22:43 +0200

    Tecnicamente esta entregable

---
+++ EjerciciosFlex/Ejercicio5/token.h
@@ -0,0 +1,64 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+#define INICIO                  257
+#define FIN                     258
+#define LEER                    259
+#define ESCRIBIR                260
+#define SI                      261
+#define ENTONCES                262
+#define SI_NO                   263
+#define FIN_SI                  264
+#define MIENTRAS                265
+#define HACER                   266
+#define FIN_MIENTRAS            267
+#define REPETIR                 268
+#define HASTA_QUE               269
+#define PARA                    270
+#define DESDE                   271
+#define HASTA                   272
+#define PASO                    273
+#define FIN_PARA                274
+
+#define ID                      275
+
+#define NUMERO                  276
+
+#define ASIGNACION              277
+#define ASIGNACIONSUMA          278
+#define ASIGNACIONRESTA         279
+#define ASIGNACIONPRODUCTO      280
+#define ASIGNACIONDIVISION      281
+
+#define SUMA                    282
+#define INCREMENTO              283
+#define RESTA                   284
+#define DECREMENTO              285
+#define PRODUCTO                286
+#define DIVISION                287
+#define DIVISION_ENTERA         288
+#define MODULO                  289
+#define POTENCIA                290
+
+#define CONCATENACION           291
+
+#define MENOR_QUE               292
+#define MENOR_IGUAL_QUE         293
+#define MAYOR_QUE               294
+#define MAYOR_IGUAL_QUE         295
+#define IGUAL                   296
+#define DISTINTO                297
+
+#define DISYUNCION_LOGICA       298
+#define CONJUNCION_LOGICA       299
+#define NEGACION_LOGICA         300
+
+#define COMENTARIO_ML_ABRE      301
+#define COMENTARIO_ML_CIERRA    302
+#define COMENTARIO_UNALINEA     303
+
+#define FIN_SENTENCIA           304
+#define PARENTESIS_IZQUIERDO    305
+#define PARENTESIS_DERECHO      306
+
+#endif
